f8f67f721a56fb735dd2c439bd79c0b536ac1df4
tkrenicky <tomas.krenicky@gmail.com>
2026-08-18 14:25:59 +0200

    Add multi-country package registry

---
+++ taxtreat/countries/__init__.py
@@ -0,0 +1,11 @@
+from taxtreat.countries.registry import (
+    CountryConfig,
+    get_country_config,
+    supported_source_countries,
+)
+
+__all__ = [
+    "CountryConfig",
+    "get_country_config",
+    "supported_source_countries",
+]
