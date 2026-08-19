c35d036ebec6ddec398ce2ea1fe972cc5d04ef8c
tkrenicky <tomas.krenicky@gmail.com>
2026-08-19 12:40:48 +0200

    Route report HTML through country localization

--- taxtreat/services/reporting/__init__.py
+++ taxtreat/services/reporting/__init__.py
@@ -1,3 +1,11 @@
 from .editorial import *
 from .client_report import *
-from .release_polish import render_report_html
+from .html_localization import localize_report_html
+from .release_polish import render_report_html as _render_release_report_html
+
+
+def render_report_html(report):
+    return localize_report_html(
+        _render_release_report_html(report),
+        report,
+    )
