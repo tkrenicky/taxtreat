ecf4ffdbd4bf0cf6058ae35ae1c359f8da8cf0a5
tkrenicky <tomas.krenicky@gmail.com>
2026-08-21 10:18:50 +0200

    Apply domestic exemption polish in report pipeline

--- taxtreat/services/reporting/__init__.py
+++ taxtreat/services/reporting/__init__.py
@@ -2,10 +2,14 @@ from .editorial import *
 from .client_report import *
 from .html_localization import localize_report_html
 from .release_polish import render_report_html as _render_release_report_html
+from .domestic_exemption_polish import apply_domestic_exemption_polish
 
 
 def render_report_html(report):
     return localize_report_html(
-        _render_release_report_html(report),
+        apply_domestic_exemption_polish(
+            _render_release_report_html(report),
+            report,
+        ),
         report,
     )
