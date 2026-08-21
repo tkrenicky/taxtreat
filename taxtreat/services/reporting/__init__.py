64adfdfeff42a1490aa3df46a64578114d1a6fef
tkrenicky <tomas.krenicky@gmail.com>
2026-08-21 10:34:37 +0200

    Wire domestic exemption report batch

--- taxtreat/services/reporting/__init__.py
+++ taxtreat/services/reporting/__init__.py
@@ -2,10 +2,22 @@ from .editorial import *
 from .client_report import *
 from .html_localization import localize_report_html
 from .release_polish import render_report_html as _render_release_report_html
+from .domestic_exemption_polish import apply_domestic_exemption_polish
+from .treaty_secondary_polish import apply_treaty_secondary_polish
+from .report_pagination_polish import apply_report_pagination_polish
 
 
 def render_report_html(report):
     return localize_report_html(
-        _render_release_report_html(report),
+        apply_report_pagination_polish(
+            apply_treaty_secondary_polish(
+                apply_domestic_exemption_polish(
+                    _render_release_report_html(report),
+                    report,
+                ),
+                report,
+            ),
+            report,
+        ),
         report,
     )
