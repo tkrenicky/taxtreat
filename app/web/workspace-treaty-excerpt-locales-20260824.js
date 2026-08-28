3a50b25c9c052de61215f929982ab5f86cf2e1c5
tkrenicky <tomas.krenicky@gmail.com>
2026-08-28 15:38:25 +0200

    Render verified English treaty rule summaries in EN UI

--- app/web/workspace-treaty-excerpt-locales-20260824.js
+++ app/web/workspace-treaty-excerpt-locales-20260824.js
@@ -24,6 +24,7 @@
     "official_treaty_text",
     "official_protocol_text",
     "official_translation_non_authentic",
+    "verified_stage6_rule_summary",
     "current_application_suspended"
   ]);
 
